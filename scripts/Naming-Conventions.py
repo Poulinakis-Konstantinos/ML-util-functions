f162477914df5d6795400ab71f9532bb248791d8
Poulinakis_Konstantinos <poulinakis.kon@gmail.com>
2021-07-19 11:37:53 +0300

    Formatting script according to PEP-8

--- scripts/Naming-Conventions.py
+++ scripts/Naming-Conventions.py
@@ -9,16 +9,22 @@ from pathlib import Path
 def extension_to_jpg(file_path):
     ''' Reformats the extension to the desired one eg. JPEG -> jpg '''
     file_name = file_path.split('.')[0]
-    print(f"Changing file extension: Renaming file : {file_path}, to {file_name}.jpg")
-    os.rename( os.path.join(dir_in, file_path) , os.path.join(dir_in,file_name+".jpg") )
+    print(
+        f"Changing file extension: Renaming file : {file_path}, to {file_name}.jpg")
+    os.rename(os.path.join(dir_in, file_path),
+              os.path.join(dir_in, file_name+".jpg"))
+
 
 def to_lower(file_path):
     ''' Changes the extension from Upper case letters to lower case letters '''
     file_name = file_path.split('.')[0]
     file_extension = file_path.split('.')[1]
     file_ext_low = file_extension.lower()
-    print(f"File extension to lower case: Renaming file : {file_path}, to {file_name}.{file_ext_low}")
-    os.rename( os.path.join(dir_in, file_path) , os.path.join(dir_in,file_name+'.'+file_ext_low) )
+    print(
+        f"File extension to lower case: Renaming file : {file_path}, to {file_name}.{file_ext_low}")
+    os.rename(os.path.join(dir_in, file_path), os.path.join(
+        dir_in, file_name+'.'+file_ext_low))
+
 
 # path to the directory you want to  check
 dir_in = r"PATH\TO\THE\DIRECTORY"
@@ -26,11 +32,8 @@ dir_in = r"PATH\TO\THE\DIRECTORY"
 for img_path in os.listdir(dir_in):
 
     # Checking for jpeg extension
-    if img_path.endswith('JPEG') or img_path.endswith('jpeg') :
+    if img_path.endswith('JPEG') or img_path.endswith('jpeg'):
         extension_to_jpg(img_path)
     # checking for upper case extension
     elif img_path.endswith('JPG'):
         to_lower(img_path)
-
-
-        
